6a82b37b11b8572a21f1270a346e238bf4f1f006
ilakonovalov <antonpuckich@gmail.com>
2024-03-28 23:40:35 +0300

    Add builder for Dataset

--- src/main/java/ru/turbogoose/cca/backend/model/Dataset.java
+++ src/main/java/ru/turbogoose/cca/backend/model/Dataset.java
@@ -1,12 +1,18 @@
 package ru.turbogoose.cca.backend.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name="datasets")
 public class Dataset {
