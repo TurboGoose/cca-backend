dad051449c0f113cc931f9d512045f78ca287beb
ilakonovalov <antonpuckich@gmail.com>
2024-03-28 23:40:42 +0300

    Add builder for Label

--- src/main/java/ru/turbogoose/cca/backend/model/Label.java
+++ src/main/java/ru/turbogoose/cca/backend/model/Label.java
@@ -1,9 +1,15 @@
 package ru.turbogoose.cca.backend.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "labels")
 public class Label {
@@ -15,6 +21,6 @@ public class Label {
     private String name;
 
     @ManyToOne
-    @JoinColumn(name = "dataset_id", nullable = false)
+    @JoinColumn(nullable = false)
     private Dataset dataset;
 }
