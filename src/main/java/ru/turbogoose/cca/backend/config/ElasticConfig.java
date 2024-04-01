9116ad5556ae66cfa032fe1c6a581e11b764ae05
ilakonovalov <antonpuckich@gmail.com>
2024-04-01 15:07:23 +0300

    Add config and bean for ElasticsearchClient

---
+++ src/main/java/ru/turbogoose/cca/backend/config/ElasticConfig.java
@@ -0,0 +1,57 @@
+package ru.turbogoose.cca.backend.config;
+
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.TransportUtils;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.elasticsearch.client.RestClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.net.ssl.SSLContext;
+
+@Configuration
+public class ElasticConfig {
+    @Value("${elasticsearch.port}")
+    private Integer port;
+
+    @Value("${elasticsearch.host}")
+    private String host;
+
+    @Value("${elasticsearch.fingerprint}")
+    private String fingerprint;
+
+    @Value("${elasticsearch.username}")
+    private String username;
+
+    @Value("${elasticsearch.password}")
+    private String password;
+
+    @Bean
+    public ElasticsearchClient elasticsearchClient() {
+        SSLContext sslContext = TransportUtils
+                .sslContextFromCaFingerprint(fingerprint);
+
+        BasicCredentialsProvider credsProv = new BasicCredentialsProvider();
+        credsProv.setCredentials(
+                AuthScope.ANY, new UsernamePasswordCredentials(username, password)
+        );
+
+        RestClient restClient = RestClient
+                .builder(new HttpHost(host, port, "https"))
+                .setHttpClientConfigCallback(hc -> hc
+                        .setSSLContext(sslContext)
+                        .setDefaultCredentialsProvider(credsProv)
+                )
+                .build();
+
+        ElasticsearchTransport transport = new RestClientTransport(restClient, new JacksonJsonpMapper());
+        return new ElasticsearchClient(transport);
+    }
+}
