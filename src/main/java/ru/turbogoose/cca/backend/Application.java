d8b5027d25836a5d64b2a5185cee9903b5ffacb2
ilakonovalov <antonpuckich@gmail.com>
2024-04-01 15:07:00 +0300

    Change elasticsearch dependency from spring data to rest client

--- src/main/java/ru/turbogoose/cca/backend/Application.java
+++ src/main/java/ru/turbogoose/cca/backend/Application.java
@@ -2,10 +2,8 @@ package ru.turbogoose.cca.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
-@EnableSpringDataWebSupport
 public class Application {
 
 	public static void main(String[] args) {
