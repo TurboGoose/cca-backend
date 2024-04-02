f797dcd6cecb4b00d6b90c0f40e570fb7f0d5152
ilakonovalov <antonpuckich@gmail.com>
2024-04-02 19:14:50 +0300

    Implement cascade label deletion

--- src/main/java/ru/turbogoose/cca/backend/model/Annotation.java
+++ src/main/java/ru/turbogoose/cca/backend/model/Annotation.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 @Data
 @Builder
@@ -18,6 +20,7 @@ public class Annotation {
 
     @MapsId("labelId")
     @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private Label label;
 }
 
