353cf8760e4935a13730aa8796a750fbfbd3e2a2
ilakonovalov <antonpuckich@gmail.com>
2024-04-03 17:56:34 +0300

    Add dto for error responses

--- src/main/java/ru/turbogoose/cca/backend/controller/GlobalControllerAdvice.java
+++ src/main/java/ru/turbogoose/cca/backend/controller/GlobalControllerAdvice.java
@@ -6,31 +6,43 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.turbogoose.cca.backend.dto.ErrorResponseDto;
+import ru.turbogoose.cca.backend.util.Commons;
 
 @RestControllerAdvice
 @Slf4j
 public class GlobalControllerAdvice {
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public void handle(IllegalArgumentException exception) {
+    public ErrorResponseDto handle(IllegalArgumentException exception) {
         log.error("", exception);
+        return composeErrorResponse(exception.getMessage());
     }
 
     @ExceptionHandler(IllegalStateException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public void handle(IllegalStateException exception) {
+    public ErrorResponseDto handle(IllegalStateException exception) {
         log.error("", exception);
+        return composeErrorResponse(exception.getMessage());
     }
 
     @ExceptionHandler(DataIntegrityViolationException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
-    public void handle(DataIntegrityViolationException exception) {
+    public ErrorResponseDto handle(DataIntegrityViolationException exception) {
         log.error("", exception);
+        String message = Commons.extractFirstPattern("Detail: (.+)\\.", exception.getMessage());
+        return composeErrorResponse(message);
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public void handle(Exception exception) {
         log.error("", exception);
+    }
+
+    private ErrorResponseDto composeErrorResponse(String message) {
+        return ErrorResponseDto.builder()
+                .message(message)
+                .build();
     }
 }
