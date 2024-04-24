97f3197e3e0403d4bfbdc703e001634456cd53de
ilakonovalov <antonpuckich@gmail.com>
2024-04-24 19:59:17 +0300

    Add StorageInfoRepository

---
+++ src/main/java/ru/turbogoose/cca/backend/components/storage/info/StorageInfoRepository.java
@@ -0,0 +1,6 @@
+package ru.turbogoose.cca.backend.components.storage.info;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StorageInfoRepository extends JpaRepository<StorageInfo, Integer> {
+}
