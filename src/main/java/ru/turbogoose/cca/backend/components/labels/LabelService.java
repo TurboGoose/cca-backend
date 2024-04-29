2ac11311c6c71495375dd927ea592bfa98fe036d
ilakonovalov <antonpuckich@gmail.com>
2024-04-29 20:23:28 +0300

    Rewrite exception handling in LabelService

--- src/main/java/ru/turbogoose/cca/backend/components/labels/LabelService.java
+++ src/main/java/ru/turbogoose/cca/backend/components/labels/LabelService.java
@@ -2,8 +2,11 @@ package ru.turbogoose.cca.backend.components.labels;
 
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ru.turbogoose.cca.backend.common.exception.AlreadyExistsException;
+import ru.turbogoose.cca.backend.common.exception.NotFoundException;
 import ru.turbogoose.cca.backend.components.datasets.Dataset;
 import ru.turbogoose.cca.backend.components.datasets.DatasetService;
 import ru.turbogoose.cca.backend.components.labels.dto.LabelListResponseDto;
@@ -19,6 +22,10 @@ public class LabelService {
     private final ModelMapper mapper;
 
     public LabelListResponseDto getLabelListForDataset(int datasetId) {
+        if (!datasetService.isDatasetExists(datasetId)) {
+            throw new NotFoundException("Dataset with this id not exists",
+                    "Dataset with this id %s not exists".formatted(datasetId));
+        }
         List<LabelResponseDto> labels = labelRepository.findAllByDatasetId(datasetId).stream()
                 .map(label -> mapper.map(label, LabelResponseDto.class))
                 .toList();
@@ -33,7 +40,13 @@ public class LabelService {
         Label label = new Label();
         label.setName(labelName);
         label.setDataset(dataset);
-        labelRepository.save(label); // integrity violation on duplicate
+        try {
+            labelRepository.save(label);
+            dataset.addLabel(label);
+        } catch (DataIntegrityViolationException exc) {
+            throw new AlreadyExistsException("Label with this name already exists",
+                    "Label with name %s already exists".formatted(labelName), exc);
+        }
         return mapper.map(label, LabelResponseDto.class);
     }
 
@@ -41,17 +54,23 @@ public class LabelService {
     public LabelResponseDto renameLabel(int labelId, String newName) {
         Label label = findLabelById(labelId);
         label.setName(newName);
-        labelRepository.save(label); // integrity violation on duplicate
+        try {
+            labelRepository.save(label);
+        } catch (DataIntegrityViolationException exc) {
+            throw new AlreadyExistsException("Label with this name already exists",
+                    "Label with name %s already exists".formatted(newName), exc);
+        }
         return mapper.map(label, LabelResponseDto.class);
     }
 
     @Transactional
     public void deleteLabel(int labelId) {
-        labelRepository.deleteById(labelId);
+        Label label = findLabelById(labelId);
+        labelRepository.delete(label);
     }
 
     public Label findLabelById(int labelId) {
         return labelRepository.findById(labelId)
-                .orElseThrow(() -> new IllegalStateException("Label with id{" + labelId + "} not found"));
+                .orElseThrow(() -> new NotFoundException("Label with id %s not found".formatted(labelId)));
     }
 }
